39efe2140c1491c7c927dcd02fe32f4ba0f2bcab
FredericSavard <450fredsavard@gmail.com>
2015-11-25 15:22:52 -0500

    cleanup, initial commit

--- ClientApp/src/main/java/com/cgi/security/oauth2/DefaultLocalAuthenticationFilter.java
+++ ClientApp/src/main/java/com/cgi/security/oauth2/DefaultLocalAuthenticationFilter.java
@@ -5,31 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpOutputMessage;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
@@ -37,13 +25,9 @@ import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
-public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProcessingFilter{
+public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
-	private OAuth2RestOperations oauth2RestTemplate;
-	
 	private static final String AUTHORIZE_URL = "https://accounts.google.com/o/oauth2/auth";
-	// private static final String ACCESS_TOKEN_URI =
-	// "https://www.googleapis.com/oauth2/v3/token";
 	private static final String ACCESS_TOKEN_URI = "https://accounts.google.com/o/oauth2/token";
 	private static final String CLIENT_SECRET = "Nrn9a7bKtwuAEmXGJFTB6WgM";
 	private static final String CLIENT_ID = "638963840149-d5p79cp00obaumgrderkmjk3dkcecv0p.apps.googleusercontent.com";
@@ -55,43 +39,33 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 		SCOPES.add("openid");
 		SCOPES.add("email");
 		SCOPES.add("profile");
-		// SCOPES.add("write");
-		// SCOPES.add("read");
-		// SCOPES.add("trust");
 	}
-	
-	
+
 	protected DefaultLocalAuthenticationFilter() {
 		super("/DefaultLocalAuthenticationFilter");
 	}
 
-
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req; 
-		
-		if(request.getSession() != null && request.getSession().getAttribute("SuperToken") != null){
-			
+		HttpServletRequest request = (HttpServletRequest) req;
+
+		if (request.getSession() != null && request.getSession().getAttribute("SuperToken") != null) {
+
 			chain.doFilter(req, res);
 			return;
 		}
-		
-		if (request.getParameter("code") != null) {
-			AuthorizationCodeAccessTokenProvider authorizationCodeAccessTokenProvider = new AuthorizationCodeAccessTokenProvider();
-			
-			
 
-			
-			//oauth2RestTemplate = new OAuth2RestTemplate(res);
+		String coreParameter = request.getParameter("code");
+		if (coreParameter != null) {
+			AuthorizationCodeAccessTokenProvider authorizationCodeAccessTokenProvider = new AuthorizationCodeAccessTokenProvider();
+
 			DefaultAccessTokenRequest accessTokenRequest = new DefaultAccessTokenRequest();
-			accessTokenRequest.setAuthorizationCode(request.getParameter("code"));
+			accessTokenRequest.setAuthorizationCode(coreParameter);
 			DefaultOAuth2ClientContext defaultOAuth2ClientContext = new DefaultOAuth2ClientContext(accessTokenRequest);
-			
+
 			AuthorizationCodeResourceDetails googleOAuth2Details = new AuthorizationCodeResourceDetails();
 
-			// googleOAuth2Details.setAuthenticationScheme(header);
-			// googleOAuth2Details.setClientAuthenticationScheme(header);
 			googleOAuth2Details.setAuthenticationScheme(AuthenticationScheme.query);
 			googleOAuth2Details.setClientAuthenticationScheme(AuthenticationScheme.form);
 			googleOAuth2Details.setClientId(CLIENT_ID);
@@ -104,40 +78,28 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 			googleOAuth2Details.setTokenName("oauth_token");
 			googleOAuth2Details.setGrantType("authorization_code");
 
-			
 			MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-			List converters = new ArrayList<HttpMessageConverter>();
+			List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
 			converters.add(converter);
 			authorizationCodeAccessTokenProvider.setMessageConverters(converters);
-			OAuth2AccessToken accessToken = authorizationCodeAccessTokenProvider.obtainAccessToken(googleOAuth2Details, accessTokenRequest);
-			
+			OAuth2AccessToken accessToken = authorizationCodeAccessTokenProvider.obtainAccessToken(googleOAuth2Details,
+					accessTokenRequest);
+
 			defaultOAuth2ClientContext.setAccessToken(accessToken);
 			request.getSession(true).setAttribute("SuperToken", accessToken);
-			Authentication authentication = new TestingAuthenticationToken("internal_system_user", "internal_null_credentials", "ROLE_USER");
-	        authentication.setAuthenticated(true);
-	        SecurityContextHolder.getContext().setAuthentication(authentication);
-			
-//			oauth2RestTemplate = new OAuth2RestTemplate(googleOAuth2Details, defaultOAuth2ClientContext);
-//		
-//			
-//			
-//			HttpHeaders headers = new HttpHeaders();
-//			headers.setContentType(MediaType.APPLICATION_JSON);
-//			//headers.setContentType(MediaType.TEXT_PLAIN);
-//			
-//			HttpEntity<String> httpEntity = new HttpEntity<String>("helloWorld", headers );	
-//			 //SecurityContextHolder.getContext().setAuthentication(attemptAuthentication((HttpServletRequest) req, (HttpServletResponse) res));
-//			final String loginResponse = oauth2RestTemplate.exchange("https://www.googleapis.com/o/oauth2",
-//					HttpMethod.POST, httpEntity, String.class).getBody();
+			Authentication authentication = new TestingAuthenticationToken("internal_system_user",
+					"internal_null_credentials", "ROLE_USER");
+			authentication.setAuthenticated(true);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
 		} else {
-	         HttpServletResponse resp = (HttpServletResponse) res;
-	         resp.setStatus(resp.SC_MOVED_PERMANENTLY);
-	         resp.setHeader("Location", "https://accounts.google.com/o/oauth2/auth?client_id=638963840149-d5p79cp00obaumgrderkmjk3dkcecv0p.apps.googleusercontent.com&redirect_uri=http://localhost:8080&response_type=code&scope=profile+email+openid");
-	         
+			HttpServletResponse resp = (HttpServletResponse) res;
+			resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+			resp.setHeader("Location",
+					"https://accounts.google.com/o/oauth2/auth?client_id=638963840149-d5p79cp00obaumgrderkmjk3dkcecv0p.apps.googleusercontent.com&redirect_uri=http://localhost:8080&response_type=code&scope=profile+email+openid");
 		}
-		
-		 
-		 chain.doFilter(req, res);
+
+		chain.doFilter(req, res);
 	}
 
 	@Override
@@ -148,9 +110,5 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 		authentication.setAuthenticated(true);
 		return getAuthenticationManager().authenticate(authentication);
 	}
-	
 
-	
-	
-	
 }
