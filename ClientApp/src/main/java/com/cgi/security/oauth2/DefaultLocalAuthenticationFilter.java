de7b63a837d30a4bc4cc9d9902ae1d01e046b30c
FredericSavard <450fredsavard@gmail.com>
2015-11-25 15:56:22 -0500

    cleanup: replace hard coded values with properties

--- ClientApp/src/main/java/com/cgi/security/oauth2/DefaultLocalAuthenticationFilter.java
+++ ClientApp/src/main/java/com/cgi/security/oauth2/DefaultLocalAuthenticationFilter.java
@@ -27,13 +27,13 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 
 public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
-	private static final String TOKEN_ATTRIBUTE = "SuperToken";
-	private static final String AUTHORIZE_URL = "https://accounts.google.com/o/oauth2/auth";
-	private static final String ACCESS_TOKEN_URI = "https://accounts.google.com/o/oauth2/token";
-	private static final String CLIENT_SECRET = "Nrn9a7bKtwuAEmXGJFTB6WgM";
-	private static final String CLIENT_ID = "638963840149-d5p79cp00obaumgrderkmjk3dkcecv0p.apps.googleusercontent.com";
-	private static final String REDIRECT_URI = "http://localhost:8080";
-	private static final String SCOPE = "profile+email+openid";
+	private String tokenAttribute;
+	private String authorizeUrl;
+	private String accessTokenUri;
+	private String clientSecret;
+	private String clientId;
+	private String redirectUri;
+	private String scope;
 	private static final List<String> SCOPES;
 
 	static {
@@ -52,7 +52,7 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 
-		if (request.getSession() != null && request.getSession().getAttribute(TOKEN_ATTRIBUTE) != null) {
+		if (request.getSession() != null && request.getSession().getAttribute(tokenAttribute) != null) {
 
 			chain.doFilter(req, res);
 			return;
@@ -62,8 +62,8 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 		if (codeParameter == null) {
 			HttpServletResponse resp = (HttpServletResponse) res;
 			resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-			resp.setHeader("Location", AUTHORIZE_URL + "?client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI
-					+ "&response_type=" + "code&scope=" + SCOPE);
+			resp.setHeader("Location", authorizeUrl + "?client_id=" + clientId + "&redirect_uri=" + redirectUri
+					+ "&response_type=" + "code&scope=" + scope);
 
 		} else {
 			final DefaultAccessTokenRequest accessTokenRequest = new DefaultAccessTokenRequest();
@@ -80,13 +80,13 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 
 		googleOAuth2Details.setAuthenticationScheme(AuthenticationScheme.query);
 		googleOAuth2Details.setClientAuthenticationScheme(AuthenticationScheme.form);
-		googleOAuth2Details.setClientId(CLIENT_ID);
-		googleOAuth2Details.setClientSecret(CLIENT_SECRET);
-		googleOAuth2Details.setUserAuthorizationUri(AUTHORIZE_URL);
-		googleOAuth2Details.setAccessTokenUri(ACCESS_TOKEN_URI);
+		googleOAuth2Details.setClientId(clientId);
+		googleOAuth2Details.setClientSecret(clientSecret);
+		googleOAuth2Details.setUserAuthorizationUri(authorizeUrl);
+		googleOAuth2Details.setAccessTokenUri(accessTokenUri);
 		googleOAuth2Details.setScope(SCOPES);
 		googleOAuth2Details.setUseCurrentUri(false);
-		googleOAuth2Details.setPreEstablishedRedirectUri(REDIRECT_URI);
+		googleOAuth2Details.setPreEstablishedRedirectUri(redirectUri);
 		googleOAuth2Details.setTokenName("oauth_token");
 		googleOAuth2Details.setGrantType("authorization_code");
 
@@ -103,7 +103,7 @@ public class DefaultLocalAuthenticationFilter extends AbstractAuthenticationProc
 				accessTokenRequest);
 		defaultOAuth2ClientContext.setAccessToken(accessToken);
 
-		request.getSession(true).setAttribute(TOKEN_ATTRIBUTE, accessToken);
+		request.getSession(true).setAttribute(tokenAttribute, accessToken);
 
 		final Authentication authentication = new TestingAuthenticationToken("internal_system_user",
 				"internal_null_credentials", "ROLE_USER");
